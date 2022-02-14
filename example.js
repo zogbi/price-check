4aea666dd89acc10c19c98c4609bd9e1df2b42cc
Fabricio Zogbi <zogbi@users.noreply.github.com>
2022-02-13 22:09:35 -0300

    Começando

---
+++ example.js
@@ -0,0 +1,39 @@
+const puppeteer = require('puppeteer');
+const fs = require('fs');
+
+
+(async () => {
+    const browser = await puppeteer.launch();
+    const page = await browser.newPage();
+    await page.goto('https://www.amazon.com.br/dp/B07ZHFM7JK/');
+    const myproductPrice = await page.$eval('#twister-plus-price-data-price', el => el.value);
+    const myproductID = await page.$eval('#ASIN', el => el.value);
+    const myproductName = await page.$eval('#productTitle', el => el.textContent);
+    //console.log(element);
+    const jsonMyProduct = {
+        productName: myproductName.trim(),
+        productID: myproductID,
+        productPrice: myproductPrice
+    };
+
+    storeData(jsonMyProduct, "sample.json");
+
+    await browser.close();
+})();
+
+const storeData = (data, path) => {
+    try {
+        fs.writeFileSync(path, JSON.stringify(data))
+    } catch (err) {
+        console.error(err)
+    }
+}
+
+const loadData = (path) => {
+    try {
+        return fs.readFileSync(path, 'utf8')
+    } catch (err) {
+        console.error(err)
+        return false
+    }
+}
