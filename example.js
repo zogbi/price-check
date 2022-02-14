210352934f95dd29b2440074df314c6dde2d2afa
Fabricio Zogbi <zogbi@users.noreply.github.com>
2022-02-14 09:13:06 -0300

    achei que ia, mas não foi

--- example.js
+++ example.js
@@ -1,25 +1,26 @@
 const puppeteer = require('puppeteer');
 const fs = require('fs');
+const merge = require('lodash.merge');
 
 
-(async () => {
+
+async function getMyAmazon(data) {
     const browser = await puppeteer.launch();
     const page = await browser.newPage();
-    await page.goto('https://www.amazon.com.br/dp/B07ZHFM7JK/');
+    console.log(data);
+    await page.goto(data);
     const myproductPrice = await page.$eval('#twister-plus-price-data-price', el => el.value);
     const myproductID = await page.$eval('#ASIN', el => el.value);
     const myproductName = await page.$eval('#productTitle', el => el.textContent);
-    //console.log(element);
     const jsonMyProduct = {
         productName: myproductName.trim(),
         productID: myproductID,
         productPrice: myproductPrice
     };
-
-    storeData(jsonMyProduct, "sample.json");
-
+    console.log(jsonMyProduct);
     await browser.close();
-})();
+    return jsonMyProduct;
+}
 
 const storeData = (data, path) => {
     try {
@@ -31,13 +32,37 @@ const storeData = (data, path) => {
 
 const loadData = (path) => {
     try {
-        return fs.readFileSync(path, 'utf8')
+        return JSON.parse(fs.readFileSync(path, 'utf8'));
     } catch (err) {
         console.error(err)
         return false
     }
 }
 
+
+(async () => {
+
+    const testeaqui = [
+        "https://www.amazon.com.br/dp/B07FNFPSWL",
+        "https://www.amazon.com.br/dp/B07LB6XSZJ",
+        "https://www.amazon.com.br/dp/B0823BN8FR",
+        "https://www.amazon.com.br/dp/B07FN7T3WD/"
+    ]
+
+    const objMyAmazon = {};
+    objMyAmazon = Object.assign(objMyAmazon, await getMyAmazon(testeaqui[0]));
+
+    objMyAmazon = Object.assign(objMyAmazon, await getMyAmazon(testeaqui[1]));
+    console.log("tentou merge: ", objMyAmazon);
+
+    console.log("Voltou:", objMyAmazon);
+
+    storeData(objMyAmazon, "sample.json");
+
+    console.log(loadData("sample.json"));
+
+
+})();
 //FUTURE
 //Slack notify
 //https://blog.nodeswat.com/simple-node-js-and-slack-webhook-integration-d87c95aa9600
