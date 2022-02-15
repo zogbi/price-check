0853672257e33a7f170901be7b98854090e88876
Fabricio Zogbi <zogbi@users.noreply.github.com>
2022-02-15 20:02:46 -0300

    agora ta quase

--- example.js
+++ example.js
@@ -1,14 +1,19 @@
 const puppeteer = require('puppeteer');
 const fs = require('fs');
-const merge = require('lodash.merge');
-
 
 
 async function getMyAmazon(data) {
-    const browser = await puppeteer.launch();
+    const browser = await puppeteer.launch({
+        headless: false,
+        args: ['--window-position=0,0 --window-size=1,1']
+    });
     const page = await browser.newPage();
     console.log(data);
     await page.goto(data);
+    // const response = await page.goto(data);
+    // const data1 = await response.buffer();
+    //console.log(page);
+    //storeData(await data1.toString('utf8'), "amazon.html");
     const myproductPrice = await page.$eval('#twister-plus-price-data-price', el => el.value);
     const myproductID = await page.$eval('#ASIN', el => el.value);
     const myproductName = await page.$eval('#productTitle', el => el.textContent);
@@ -18,6 +23,8 @@ async function getMyAmazon(data) {
         productPrice: myproductPrice
     };
     console.log(jsonMyProduct);
+
+    await sleep(5000);
     await browser.close();
     return jsonMyProduct;
 }
@@ -39,25 +46,35 @@ const loadData = (path) => {
     }
 }
 
+function sleep(ms) {
+    return new Promise((resolve) => {
+        setTimeout(resolve, ms);
+    });
+}
 
 (async () => {
 
-    const testeaqui = [
+    const myProductsCheck = [
         "https://www.amazon.com.br/dp/B07FNFPSWL",
         "https://www.amazon.com.br/dp/B07LB6XSZJ",
         "https://www.amazon.com.br/dp/B0823BN8FR",
         "https://www.amazon.com.br/dp/B07FN7T3WD/"
     ]
 
-    const objMyAmazon = {};
-    objMyAmazon = Object.assign(objMyAmazon, await getMyAmazon(testeaqui[0]));
+    var arrMyAmazon = [];
+    myProductsCheck.forEach(element => {
+        console.log(element);
+        arrMyAmazon.push(await getMyAmazon(element));
+        sleep(5000);
+    });
 
-    objMyAmazon = Object.assign(objMyAmazon, await getMyAmazon(testeaqui[1]));
-    console.log("tentou merge: ", objMyAmazon);
 
-    console.log("Voltou:", objMyAmazon);
+    //arrMyAmazon.push(await getMyAmazon(testeaqui[1]));
+    console.log("tentou merge: ", arrMyAmazon);
 
-    storeData(objMyAmazon, "sample.json");
+    console.log("Voltou:", arrMyAmazon);
+
+    storeData(arrMyAmazon, "sample.json");
 
     console.log(loadData("sample.json"));
 
